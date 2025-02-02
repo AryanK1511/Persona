138130606532f0dc777239f03e8fa18e65cb43c6
Aryan Khurana <aryankhurana1511@gmail.com>
2025-02-02 10:23:29 +0000

    chore: hardware stuff

--- hardware/listener.py
+++ hardware/listener.py
@@ -1,52 +1,9 @@
-<<<<<<< HEAD
-import assemblyai as aai
-
-aai.settings.api_key = "8f2ee51c6a8d4413b9b486f44afec4ae"
-
-
-def on_open(session_opened: aai.RealtimeSessionOpened):
-    print("Session ID:", session_opened.session_id)
-
-
-def on_data(transcript: aai.RealtimeTranscript):
-    if not transcript.text:
-        return
-
-    if isinstance(transcript, aai.RealtimeFinalTranscript):
-        print(transcript.text, end="\r\n")
-    else:
-        print(transcript.text, end="\r")
-
-
-def on_error(error: aai.RealtimeError):
-    print("An error occurred:", error)
-
-
-def on_close():
-    print("Closing Session")
-
-
-transcriber = aai.RealtimeTranscriber(
-    sample_rate=16_000,
-    on_data=on_data,
-    on_error=on_error,
-    on_open=on_open,
-    on_close=on_close,
-)
-
-transcriber.connect()
-
-microphone_stream = aai.extras.MicrophoneStream(sample_rate=16_000)
-transcriber.stream(microphone_stream)
-
-transcriber.close()
-=======
 import os
 import sys
 
+import requests
 import speech_recognition as sr
 from dotenv import load_dotenv
-from workflow import Workflow
 
 load_dotenv()
 
@@ -62,7 +19,6 @@ def detect_silence(audio_data, recognizer, silence_threshold=1.0):
 
 
 def main():
-    ws = Workflow()
     r = sr.Recognizer()
     r.dynamic_energy_threshold = True
     r.dynamic_energy_adjustment_damping = 0.15
@@ -82,9 +38,12 @@ def main():
 
                 result = {"message": transcription}
                 print(result)
-                ws.run("123", transcription)
 
-                # Once we get the response from the workflow, we need to send that response to the other pi
+                response = requests.post(
+                    "http://localhost:8000/log_message", json=result
+                )
+                if response.status_code == 200:
+                    print("Message logged successfully")
 
             except sr.WaitTimeoutError:
                 print("No speech detected")
@@ -96,4 +55,3 @@ def main():
 
 if __name__ == "__main__":
     main()
->>>>>>> 194597baea80a2118a376135cd8913217a248ab8
--- hardware/workflow/__init__.py
+++ hardware/workflow/__init__.py
@@ -1,47 +1,5 @@
-<<<<<<< HEAD
-# import os
+import os
 
-# from dotenv import load_dotenv
-# load_dotenv()
-# DEBUGGING_FLAG = os.getenv("DEBUGGING")
-
-# from intent import Intent
-# from llm import LLM
-# from permission import Permission
-# from stt import STT
-# from tts import TTS
-
-# class Workflow:
-#     def __init__(self):
-#         # maybe all of these are not used, reomove if not used
-#         self.intent = Intent()
-#         self.llm = LLM()
-#         self.permission = Permission()
-#         self.stt = STT()
-#         self.tts = TTS()
-
-#     def run(self, user_id, prompt):
-#         if DEBUGGING_FLAG:
-#             print("1. workflow started with user_id: ", user_id, " and prompt: ", prompt)
-
-#         intent = self.intent.get_intent(prompt)
-#         if DEBUGGING_FLAG:
-#             print("2. intent: ", intent)
-
-#         permission_error = self.permission.check_permission(user_id, intent)
-#         if DEBUGGING_FLAG:
-#             print("3. permission_error: ", permission_error)
-
-#         if permission_error:
-#             return {"error": permission_error}
-        
-#         system_message = "orange"
-#         response = self.llm.generate_response(prompt, intent, system_message)
-#         if DEBUGGING_FLAG:
-#             print("4. response: ", response)
-
-#         return {"response": response}
-=======
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -96,7 +54,6 @@ class Workflow:
         if DEBUGGING_FLAG:
             print("5. audio_url: ", audio_url)
         return {"response": audio_url}
->>>>>>> 194597baea80a2118a376135cd8913217a248ab8
 
 
 # test = Workflow()
